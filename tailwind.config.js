568f48167b79017d807a31b0f3bcc07ead81d8f6
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2023-10-21 21:48:04 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        grey: '#DFDEE0',
+      },
+    },
+  },
+  plugins: [],
+};
